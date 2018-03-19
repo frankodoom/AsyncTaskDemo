5748ff889b1f343c7b7e49f7b5d8b6c7098f5711
Frank Odoom <f.odoom@outlook.com>
2018-03-19 07:47:37 +0000

    initial commit

---
+++ AsyncDemo/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace AsyncDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSynch = new System.Windows.Forms.Button();
+            this.btnAsynch = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSynch
+            // 
+            this.btnSynch.Location = new System.Drawing.Point(220, 283);
+            this.btnSynch.Name = "btnSynch";
+            this.btnSynch.Size = new System.Drawing.Size(344, 87);
+            this.btnSynch.TabIndex = 0;
+            this.btnSynch.Text = "Run Synchronous Task";
+            this.btnSynch.UseVisualStyleBackColor = true;
+            this.btnSynch.Click += new System.EventHandler(this.btnSynch_Click);
+            // 
+            // btnAsynch
+            // 
+            this.btnAsynch.Location = new System.Drawing.Point(220, 406);
+            this.btnAsynch.Name = "btnAsynch";
+            this.btnAsynch.Size = new System.Drawing.Size(344, 87);
+            this.btnAsynch.TabIndex = 1;
+            this.btnAsynch.Text = "Run Asynchronuous Task";
+            this.btnAsynch.UseVisualStyleBackColor = true;
+            this.btnAsynch.Click += new System.EventHandler(this.btnAsynch_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(275, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(222, 198);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(279, 544);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "By Frank Odoom";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 625);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnAsynch);
+            this.Controls.Add(this.btnSynch);
+            this.Name = "Form1";
+            this.Text = "Asynchronous Demo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSynch;
+        private System.Windows.Forms.Button btnAsynch;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AsyncDemo/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private  void btnSynch_Click(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync("http://www.bing.com");
+            MessageBox.Show(response.Result.ToString());
+        }
+
+        private async void btnAsynch_Click(object sender, EventArgs e)
+        {
+             await Browse("http://www.bing.com");
+        }
+
+
+
+        public async Task Browse(string url)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            var threadId =Thread.CurrentThread.IsThreadPoolThread;
+            MessageBox.Show("Request Returned Http StatusCode:" +  response.StatusCode + " using Thread :  "   + threadId);
+        }
+    }
+}
+                                         
